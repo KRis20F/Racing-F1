7e3799f3ddd7ee14e7b6988016262993ad20b09e
KRis20F <krigutierrez009@gmail.com>
2025-05-13 14:21:07 +0200

    fix app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,11 @@
-import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "../routes/index";
-import Navbar from "./UI/Navbar";
 
 export function App() {
   return (
     <div className="min-h-screen">
-      <Router>
-        <Navbar />
-        <main className="container mx-auto px-4">
-          <Routes />
-        </main>
-      </Router>
+      <main className="container mx-auto px-4">
+        <Routes />
+      </main>
     </div>
   );
 }
